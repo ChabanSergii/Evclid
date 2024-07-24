00f83777e0e686c1184c7b0097b1f9757c5a6824
chaban <chabchabik@gmail.com>
2024-07-24 20:46:54 +0300

    added burger menu 1024

--- initial.js
+++ initial.js
@@ -1,33 +1,40 @@
 
-/* Hero swiperslider */
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay: true,
-    speed: 500,
+window.addEventListener("DOMContentLoaded", function() {
+  this.document.querySelector('#burger').addEventListener('click', function() {
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    
-});
+  this.document.querySelector('#burger-hidden').addEventListener('click', function() {
+    document.querySelector('#menu').classList.toggle('is-active')
+  })
 
+  /* Hero swiperslider */
+  const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      autoplay: true,
+      speed: 500,
 
-/* How jqueryui tabs */  
-$( function() {
-$( "#tabs" ).tabs({
-    event: "mouseover",
-    active: 0,
-    heightStyle: "auto",
-});
-} );
+      // If we need pagination
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+      },
+      
+  });
 
+  /* How jqueryui tabs */  
+  $( function() {
+    $( "#tabs" ).tabs({
+        event: "mouseover",
+        active: 0,
+        heightStyle: "auto",
+    });
+  } );
 
-/* Faq jqueryui accordion */
-$( function() {
+  /* Faq jqueryui accordion */
+  $( function() {
     $( "#accordion" ).accordion({
       active: 0,
       animate: {
@@ -37,3 +44,5 @@ $( function() {
       icons: false,
     });
   });
+
+})
